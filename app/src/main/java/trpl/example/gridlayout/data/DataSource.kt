d114e778c708dcbb0dc09f9568b52de62a99be95
LAPTOP-AVUPTM01\A.S Rizqi <rizqiaguspu@gmail.com>
2024-12-13 15:04:18 +0700

    NIM 234311053

---
+++ app/src/main/java/trpl/example/gridlayout/data/DataSource.kt
@@ -0,0 +1,18 @@
+package trpl.example.gridlayout.data
+
+import trpl.example.gridlayout.R
+import trpl.example.gridlayout.model.Topic
+
+object DataSource {
+    val topics = listOf(
+        Topic(R.string.aap, 58, R.drawable.aap),
+        Topic(R.string.allister, 30, R.drawable.cristiano),
+        Topic(R.string.cristiano, 90, R.drawable.cristiano),
+        Topic(R.string.idzes, 121, R.drawable.idzes),
+        Topic(R.string.lautaro, 78, R.drawable.lautaro),
+        Topic(R.string.merino, 118, R.drawable.merino),
+        Topic(R.string.pele, 423, R.drawable.pele),
+        Topic(R.string.phelp, 28, R.drawable.phelp),
+        Topic(R.string.sergio, 67, R.drawable.sergio),
+    )
+}
